c359a00f97dc94cc842ea54071044becfba46edd
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-10-14 21:38:32 -0300

    Configurando tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default';
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button scheme="primary" />
       <Button scheme="secondary" />
       <Button scheme="danger" />
       <Button scheme="success" />
       <Button />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+    white: '#fdfdfd',
+    primary: 'purple',
+    secondary: 'orange'
+}
